ab3ae61d3f5d95a5789b17f39f1cd52a8b441910
Gita <11582g@gmail.com>
2026-03-20 03:36:18 +0200

    net10/

--- backend/obj/Debug/net10.0/Achim.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/Achim.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Achim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1e7449250212a41f0ddf4575f63539901bad2b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+857205f8a8551274b57c39085fb50c0058eabbf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Achim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Achim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
